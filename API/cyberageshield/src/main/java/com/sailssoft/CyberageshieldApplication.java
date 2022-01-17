e15e2178b9335ea5183496c4fda8b87cc072d9d3
vvrkFSD <ramakrishna.vasamsetti@sailssoftware.com>
2022-01-17 11:57:23 +0530

    Initial Commit

---
+++ API/cyberageshield/src/main/java/com/sailssoft/CyberageshieldApplication.java
@@ -0,0 +1,13 @@
+package com.sailssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberageshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberageshieldApplication.class, args);
+	}
+
+}
