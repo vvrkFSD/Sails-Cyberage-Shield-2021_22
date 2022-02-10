3070da7be1d229f00c1b4150001984454916b517
Meghana Y <meghanayalla07@gmail.com>
2022-02-10 10:59:45 +0530

    Startpage(dashboard)
    worked on start page of the website

--- GUI/src/app/app.module.ts
+++ GUI/src/app/app.module.ts
@@ -8,10 +8,14 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './common-components/login/login.component';
 import { ForgotPasswordComponent } from './common-components/forgot-password/forgot-password.component';
 import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ManageUsersComponent } from './admin/manage-users/manage-users.component'
 import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
+<<<<<<< Updated upstream
 import { DashboardComponent } from './common-components/dashboard/dashboard.component';
+=======
+import { ResetpasswordComponent } from './common-components/resetpassword/resetpassword.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { DashboardComponent } from './common-components/dashboard/dashboard.comp
     ForgotPasswordComponent,
     AdminHeaderComponent,
     ManageUsersComponent,
+<<<<<<< Updated upstream
     DashboardComponent
+=======
+    ResetpasswordComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
